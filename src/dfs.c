03c2ae860b893c5039bd71295be698c61e5ad764
phatphamdk <phat_pm@hotmail.com>
2021-11-08 15:47:32 +0100

    DFS

--- src/dfs.c
+++ src/dfs.c
@@ -6,22 +6,42 @@
 #include <stdlib.h>		/* abort */
 #include <stdbool.h>		/* bool, true, false */
 #include "dfs.h"
+#include <assert.h>
 
 
-void DFT (node * root)
-{
-	// Implement DFS
-	// Hint: You can use print_node, print_tree and/or print_stack.
+void DFT (node * root){
+  assert(root != NULL);                                         // If root == null the tree is empty
+  stack *temp = (struct stack *)malloc(sizeof(struct stack *)); // Make stack named 'temp'
+  temp = push(temp, root);                                      // Push root onto stack
+
+  while (temp != NULL)
+  {
+    node *toppp = temp->node;
+    temp = pop(temp);
+
+    if (toppp->rchild != NULL){
+      temp = push(temp, toppp->rchild); // Pushing right child onto stack
+    }
+    if (toppp->lchild != NULL){ // Pushing left child onto stack
+      temp = push(temp, toppp->lchild);
+    }  
+  }
 }
 
-node *make_node (int num, node * left, node * right)
-{
-	return 0;
+node *make_node (int num, node * left, node * right){
+  struct node *n;                                 // creating a node pointer
+  n = (struct node *)malloc(sizeof(struct node)); // Allocating memory in the heap
+  n->num = num;                                   // Setting the data
+  n->lchild = left;                               // Setting the left children to left
+  n->rchild = right;                              // Setting the right children to right
+  return n;                                       // Finally returning the created node
 }
 
-void free_node (node * p)
-{
-	
+
+void free_node (node * p){
+  free_node(p->lchild);
+  free_node(p->rchild);
+  free(p);
 }
 
 
@@ -56,14 +76,16 @@ void print_tree (node * p, int depth)
     print_tree (p->rchild, depth + 1);
 }
 
-stack *push (stack * topp, node * node)
-{
-	return 0;
+stack *push (stack * topp, node * node){ 
+  struct stack *temp = (struct stack *)malloc(sizeof(struct stack *));
+  temp->node = newnode;
+  temp->next = topp;
+  return temp;
 }
 
 bool isEmpty (stack * topp)
 {
-  return false;
+  return topp = NULL;
 }
 
 node *top (stack * topp)
@@ -74,9 +96,12 @@ node *top (stack * topp)
 // Utility function to pop topp  
 // element from the stack 
 
-stack *pop (stack * topp)
-{
-	return 0;
+stack *pop (stack * topp){
+  stack *toppp;
+  printf("%d, ", topp->node->num);
+  toppp = topp->next; //Popping the top element.
+  free(topp);
+  return toppp;
 }
 
 void print_stack (stack * topp)
